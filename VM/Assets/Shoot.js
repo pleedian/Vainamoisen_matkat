b9204b8f0616b8269b2f2c436e6970565914fd52
pleedian <pleedian@gmail.com>
2016-04-20 00:15:15 +0300

    Player shoot
    - basic shooting

---
+++ VM/Assets/Shoot.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+  //shoot();
+}
+
+function Update () {
+
+}
+
+//function to shoot:
+var ammo: GameObject;
+function shoot(){
+  var player =  GameObject.FindGameObjectWithTag("Player");
+  var gun = player.transform.Find("gun").gameObject;
+  var bullet = Instantiate(ammo,gun.transform.position, Quaternion.identity);
+  bullet.GetComponent.<Rigidbody2D>().AddForce(Vector2.right * 250);
+}
